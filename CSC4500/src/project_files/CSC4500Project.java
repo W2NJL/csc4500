3205b92f4e82e42fb912bf8798fa20d247776bab
Nick Langan <55940428+W2NJL@users.noreply.github.com>
2022-01-25 10:56:46 -0500

    Additional formatting/indentation

--- CSC4500/src/project_files/CSC4500Project.java
+++ CSC4500/src/project_files/CSC4500Project.java
@@ -1,7 +1,6 @@
 // CSC 4500 Programming Project
 // Use this file to execute all assignments
 
-
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
@@ -20,43 +19,36 @@ import csc4500.core.search.framework.qsearch.GraphSearch;
 import csc4500.core.search.informed.AStarSearch;
 import csc4500.core.search.informed.GreedyBestFirstSearch;
 
-
 /**
- * @author YOUR NAME HERE
- * Code adapted for Villanova CSC 4500 Project assignment
- * 
+ * @author YOUR NAME HERE Code adapted for Villanova CSC 4500 Project assignment
+ *
  */
 
 public class CSC4500Project {
-	
-	
-	//EightPuzzle Boards are constructed here
-	static EightPuzzleBoard boardWithThreeMoveSolution = new EightPuzzleBoard(new int[] { 1, 2, 5, 3, 4, 0, 6, 7, 8 });;
+
+	// EightPuzzle Boards are constructed here
+	static EightPuzzleBoard boardWithThreeMoveSolution = new EightPuzzleBoard(new int[] { 1, 2, 5, 3, 4, 0, 6, 7, 8 });
 
 	static EightPuzzleBoard random1 = new EightPuzzleBoard(new int[] { 1, 4, 2, 7, 5, 8, 3, 0, 6 });
 
 	static EightPuzzleBoard extreme = new EightPuzzleBoard(new int[] { 0, 8, 7, 6, 5, 4, 3, 2, 1 });
 
 	public static void main(String[] args) {
-		
-		
-	//Here is where you call each search algorithm function, 
-    //with the arguments provided of eight your puzzle board and Heuristic Function
+
+		// Here is where you call each search algorithm function,
+		// with the arguments provided of eight your puzzle board and Heuristic Function
 		eightPuzzleAStarDemo(random1, new MisplacedTilleHeuristicFunction());
 		eightPuzzleAStarManhattanDemo(extreme, new ManhattanHeuristicFunction());
 
 	}
 
-
-
-	//Within each search function, the Heuristic Function can be changed
+	// Within each search function, the Heuristic Function can be changed
 	private static void eightPuzzleGreedyBestFirstDemo(EightPuzzleBoard puzzleBoard, HeuristicFunction hf) {
 		System.out.println("\nEightPuzzleDemo Greedy Best First Search (MisplacedTileHeursitic)-->");
 		try {
 			Problem problem = new Problem(puzzleBoard, EightPuzzleFunctionFactory.getActionsFunction(),
 					EightPuzzleFunctionFactory.getResultFunction(), new EightPuzzleGoalTest());
-			SearchForActions search = new GreedyBestFirstSearch(new GraphSearch(),
-					hf);
+			SearchForActions search = new GreedyBestFirstSearch(new GraphSearch(), hf);
 			SearchAgent agent = new SearchAgent(problem, search);
 			printActions(agent.getActions());
 			printInstrumentation(agent.getInstrumentation());
@@ -90,14 +82,12 @@ public class CSC4500Project {
 			SearchAgent agent = new SearchAgent(problem, search);
 			printActions(agent.getActions());
 			printInstrumentation(agent.getInstrumentation());
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
-
-
 
 	private static void eightPuzzleAStarManhattanDemo(EightPuzzleBoard puzzleBoard, HeuristicFunction hf) {
 		System.out.println("\nEightPuzzleDemo AStar Search (ManhattanHeursitic)-->");
@@ -125,8 +115,8 @@ public class CSC4500Project {
 	}
 
 	private static void printActions(List<Action> actions) {
-		for (int i = 0; i < actions.size(); i++) {
-			String action = actions.get(i).toString();
+		for (Action action2 : actions) {
+			String action = action2.toString();
 			System.out.println(action);
 		}
 	}
